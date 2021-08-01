3f575f8dd270efb339d1d465fff0a391b366ce71
Pravin <pravin.borate97@gmail.com>
2021-08-01 22:59:31 +0530

    Python package for text preprocessing

---
+++ README.md
@@ -0,0 +1,5 @@
+# Preprocessing Text Python Package
+
+We can use this package for any kind of text Processing
+
+This text procession python package prepared by **Pravin Borate**.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open('README.md','r') as file:
+    long_description = file.read()
+
+
+setuptools.setup(name='text_preprocess_package', #This should be unique
+version = '0.0.1',
+author = 'Pravin Borate',
+author_email = '1pravin.borate@gmail.com',
+description = 'This is text Preprocessing package',
+Long_description = long_description,
+long_description_content_type = 'text/markdown',
+packages = setuptools.find_packages(),
+classifiers = [
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires '>=3.5'
+)
